4324b52747de6a30d8eaab0add60627796005bbf
Sean West <sean.e.west@gmail.com>
2017-08-31 21:48:01 -0600

    list route checks path and HTTP method

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 var http = require('http');
+var url = require('url');
 
 var host = process.env['HOST'] || '127.0.0.1';
 var port = process.env['PORT'] || 3000;
 
 var todos = ['sweep floors', 'take out rubbish'];
 
-var server = http.createServer(function(req, res) {
+function router(req, res) {
+  console.log(req.socket.remoteAddress + ': ' + req.method + ' ' + req.url);
+
+  var pathname = url.parse(req.url).pathname;
+  if (pathname === '/' && req.method === 'GET') {
+    list(req, res);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}
+
+function list(req, res) {
   var headers = {
     'Content-Type': 'application/json',
     'X-Content-Type-Options': 'nosniff'
@@ -16,7 +29,9 @@ var server = http.createServer(function(req, res) {
 
   res.writeHead(200, headers);
   res.end(body);
-});
+}
+
+var server = http.createServer(router);
 
 server.listen(port, host, function() {
   console.log('Listening on port ' + port + ' and host ' + host);
